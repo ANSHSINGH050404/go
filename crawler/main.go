21b99c70c2d05dba50d807caab445c6b1ed545b5
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-03-31 11:59:47 +0530

    added some data

---
+++ crawler/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func main() {
+
+	const url = "mongodb://localhost:27017/gocrawler"
+
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(url).SetServerAPIOptions(serverAPI)
+
+	// 3. Create a new client and connect
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 4. Disconnect when the program ends
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// 5. Ping the database to verify connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("Could not connect to MongoDB:", err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB!")
+
+}
--- gin-quickstart/main.go
+++ gin-quickstart/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
+	
+	r := gin.Default()
 
-	// Query string parameters are parsed using the existing underlying request object.
-	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
-	router.GET("/welcome", func(c *gin.Context) {
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
-
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	r.GET("/ping", func(c  *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
 	})
-	router.Run(":8080")
+
+	r.Run()
 }
